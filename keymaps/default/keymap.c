dbf1270cab00717e3a81f81f9282a40897af5bb3
pabile <1162412+pabile@users.noreply.github.com>
2019-12-22 13:31:11 +0800

    Update keymap.c

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -2,7 +2,12 @@
 #include "backlight.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_ortho(KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_P7, KC_P8, KC_P9, KC_PLUS, KC_P4, KC_P5, KC_P6, KC_TAB, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_P0, KC_PCMM, KC_PDOT, 	KC_PENT)};
+	[0] = LAYOUT_ortho(
+		KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
+		KC_P7,   KC_P8,   KC_P9,   KC_PPLS, 
+		KC_P4,   KC_P5,   KC_P6,   KC_TAB, 
+		KC_P1,   KC_P2,   KC_P3,   KC_PEQL, 
+		KC_P0,   KC_PCMM, KC_PDOT, KC_PENT)};
 
 void encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) { /* First encoder */
